856815402f56737b269abd5f7900f6f8519adb09
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2024-03-03 17:36:00 +0600

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,13 @@
+
+// Add fade-in animation to each activity
+const activities = document.querySelectorAll('.activity');
+
+activities.forEach(activity => {
+    activity.classList.add('fade-in');
+});
+
+
+
 function toggleExpansion(button) {
   const card = button.closest('.skill-card');
   card.classList.toggle('expanded');
@@ -6,77 +16,11 @@ function toggleExpansion(button) {
   additionalText.style.display = additionalText.style.display === 'block' ? 'none' : 'block';
 }
 
+document.querySelector(".nav__toggle__icon").addEventListener('click', function() {
+  document.querySelector('.nav__list').classList.toggle('active');
+});
 
 
 
-const data = {
-  labels: ['Hard', 'Easy', 'Medium'],
-  datasets: [{
-    label: 'Number of Solved',
-    data: [4, 28, 85],
-    backgroundColor: [
-      'rgba(255, 26, 104, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(0, 0, 0, 0.2)'
-    ],
-    borderColor: [
-      'rgba(255, 26, 104, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)',
-      'rgba(0, 0, 0, 1)'
-    ],
-    borderWidth: 1,
-    hoverOffset: 20
-  }]
-};
 
-const config = {
-  type: 'pie',
-  data,
-  options: {
-      layout: {
-          padding: 20
-      },
-  }
-};
 
-// render init block
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
-
-// Instantly assign Chart.js version
-const chartVersion = document.getElementById('chartVersion');
-chartVersion.innerText = Chart.version;
-
-var mybutton = document.getElementById("myBtn");
-
-// Function to show the button when scrolling down 20px
-function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  mybutton.style.display = "block";
-} else {
-  mybutton.style.display = "none";
-}
-}
-
-// Add an event listener to the window object, 
-// calling the scrollFunction() whenever the page is scrolled
-window.onscroll = function() { scrollFunction(); };
-
-// Function to scroll to the top of the page when the button is clicked
-function topFunction() {
-document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-// Add an event listener to the button, 
-// calling the topFunction() when the button is clicked
-mybutton.addEventListener("click", topFunction);
